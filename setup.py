d1e93fb2e800b355893510e6e3a552a946ec7545
Marco Capypara Köpcke <hello@capypara.de>
2023-10-17 18:35:28 +0200

    switch to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,39 +1,9 @@
-__version__ = '1.6.1'
-
 from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
-# README read-in
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
-# END README read-in
-
 setup(
     name="skytemple-ssb-emulator",
-    version=__version__,
     rust_extensions=[RustExtension(f"skytemple_ssb_emulator", binding=Binding.PyO3)], # set debug=True for unoptimized build.
     packages=["skytemple_ssb_emulator"],
-    package_data={"skytemple_ssb_emulator": ["py.typed", "*.pyi"]},
-    install_requires=[
-        'range-typed-integers >= 1.0.0'
-    ],
-    description='Emulator runtime and bindings for skytemple-ssb-debugger.',
-    long_description=long_description,
-    long_description_content_type='text/x-rst',
-    url='https://github.com/SkyTemple/skytemple-ssb-emulator/',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Programming Language :: Python',
-        'Programming Language :: Rust',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-    ],
-    # rust extensions are not zip safe, just like C-extensions.
     zip_safe=False,
-    include_package_data=True,
 )
